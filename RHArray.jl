c178adf28d251c6f7cfeb59eb0178cd9c3435f3c
Devansh0210 <devansht0210@gmail.com>
2021-03-17 17:35:13 +0530

    modified little bit

--- RHArray.jl
+++ RHArray.jl
@@ -5,7 +5,7 @@ function RArray(v::Vector)
 	M[1,1:end] = [v[i] for i in 1:2:N]
 	M[2,1:end-(N%2)] = [v[i] for i in 2:2:N]
 	for i in 3:N
-		M[i,1:end-1] = [(M[i-1,1]*M[i-2,j+1] - M[i-1,j+1]*M[i-2,j])/M[i-1,1] for j in 						1:xlim-1]
+		M[i,1:end-1] = [(M[i-1,1]*M[i-2,j+1] - M[i-1,j+1]*M[i-2,j])/M[i-1,1] for j in xlim]
 	end
 	
 	return M
