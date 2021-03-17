700af4008844632e71993a29fb56be6e68d528b3
Devansh0210 <devansht0210@gmail.com>
2021-03-17 18:16:20 +0530

    resolved case where element is zero

--- RHArray.jl
+++ RHArray.jl
@@ -4,8 +4,9 @@ function RArray(v::Vector)
 	xlim = Int(ceil(N/2))
 	M[1,1:end] = [v[i] for i in 1:2:N]
 	M[2,1:end-(N%2)] = [v[i] for i in 2:2:N]
-	for i in 3:N
-		M[i,1:end-1] = [(M[i-1,1]*M[i-2,j+1] - M[i-1,j+1]*M[i-2,j])/M[i-1,1] for j in 1:xlim]
+	
+      for i in 3:N
+		M[i,1:end-1] = [(M[i-1,1]*M[i-2,j+1] - M[i-1,j+1]*M[i-2,j])/M[i-1,1] ==0 ? eps(Float64) : (M[i-1,1]*M[i-2,j+1] - M[i-1,j+1]*M[i-2,j])/M[i-1,1] for j in 						1:xlim-1]
 	end
 
 	sc = 0
@@ -14,5 +15,5 @@ function RArray(v::Vector)
 	end
 	return sc
 	# return M
-      
+
 end
